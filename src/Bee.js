d62ec002b01d615d314a13da28377ee92c4cc1a3
Suhail Idrees <gusmanbhai@hotmail.com>
2016-11-07 15:25:19 -0800

    Complete first exercise

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,32 @@
 var Bee = function() {
+	Grub.call(this);
+	
+	//Overwrite from superclass
+	this.age = 5;
+	this.color = 'yellow';
+	
+	//Inherited from superclass
+	this.food = grub.food;
+	this.eat = grub.eat;
+	
+	//new property
+	this.job = 'keep on growing';
+
+
 };
+
+
+// Make the link between the superclass and the subclass
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Grub;
+
+
+
+	
+
+
+
+
+
 
